29a584a47bc2b378bf7581f1d697317427879096
Jitgitbit <rowowolf@gmail.com>
2020-03-11 18:40:33 +0100

    identified delete fn and passed down to cmpnt

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ class App extends React.Component {
       {id:2, content: 'pyramid exercise'}
     ]
   }
+  deleteTodo = (id) => {
+    console.log(id)
+  }
   render(){
     return (
       <div className="todo-app container">
         <h1 className='center blue-text'>Todo's</h1>
-        <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     );
   }  
